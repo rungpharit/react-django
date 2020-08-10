18f1a9bc592bd0400cba86dce1181943e6d5c241
Ong <Ong@gmail.com>
2020-08-10 22:07:24 +0700

    update api

--- backend/todolist/todos/api/urls.py
+++ backend/todolist/todos/api/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+#from django.urls import path
 
-from .views import TodosListView, TodoDetailView
+# from .views import TodosListView, TodosCreateView, TodosUpdateView, TodosDeleteView
 
-urlpatterns = [
-  path('',TodosListView.as_view())
-]
+# urlpatterns = [
+#   path('',TodosListView.as_view()),
+#   path('create/',TodosCreateView.as_view()),
+#   path('<pk>/update/',TodosUpdateView.as_view()),
+#   path('<pk>/delete/',TodosDeleteView.as_view()),
+# ]
+
+from todos.api.views import TodosViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', TodosViewSet, basename='todos')
+urlpatterns = router.urls
--- backend/todolist/todos/api/views.py
+++ backend/todolist/todos/api/views.py
@@ -1,12 +1,27 @@
-from rest_framework.generics import ListAPIView,RetrieveAPIView
+# from rest_framework.generics import ListAPIView,CreateAPIView,UpdateAPIView,DestroyAPIView
 
 from todos.models import Todos
 from .serializers import TodosSerializer
 
-class TodosListView(ListAPIView) :
-  queryset = Todos.objects.all()
-  serializer_class = TodosSerializer
+# class TodosListView(ListAPIView) :
+#   queryset = Todos.objects.all()
+#   serializer_class = TodosSerializer
 
-class TodoDetailView(RetrieveAPIView):
-  queryset = Todos.objects.all()
+# class TodosCreateView(CreateAPIView) :
+#   queryset = Todos.objects.all()
+#   serializer_class = TodosSerializer
+
+# class TodosUpdateView(UpdateAPIView) :
+#   queryset = Todos.objects.all()
+#   serializer_class = TodosSerializer
+
+# class TodosDeleteView(DestroyAPIView) :
+#   queryset = Todos.objects.all()
+#   serializer_class = TodosSerializer
+
+from rest_framework import viewsets
+
+class TodosViewSet(viewsets.ModelViewSet):
   serializer_class = TodosSerializer
+  queryset = Todos.objects.all()
+
